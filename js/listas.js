de42f8d36e972613f987a7cf5eacdf69885eaadc
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-04-04 22:33:20 -0500

    Update listas.js

--- js/listas.js
+++ js/listas.js
@@ -1,16 +1,22 @@
 function mostrarLista(listaPokes){
-    document.getElementById("app")
-    app.innerHTML = "listas"
-    let Pokes = "";
+    const app = document.getElementById("app");
+    
+    let Pokes = " ";
 
+    const seccion = document.createElement("section");
+    seccion.classList.add("c-lista"); 
 
-for (let i = 0; i < listaPokes.length; i++) {
-    let id = listaPokes[i].url.split("/")[6];
-    Pokes += `
-    <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listaPokes[i].name}')">
-        <p>#${id}</p>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listaPokes[i].name}">
-        <p>${listaPokes[i].name}</p>
-    </div>`;
+    for (let i = 0; i < listaPokes.length; i++) {
+        let id = listaPokes[i].url.split("/")[6];
+        Pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listaPokes[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listaPokes[i].name}">
+            <p>${listaPokes[i].name}</p>
+        </div>`;
     }
+    seccion.innerHTML = Pokes;
+    
+    /*app.appendChild(buscador);*/
+    app.appendChild(seccion);
 }
